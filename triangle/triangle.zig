df54eb04282bae2a1a99379eb8efe025bb2d03c6
Vladislav Savonov <vladislav.s@rakia.ai>
2026-07-21 14:12:11 +0400

    Complete triangle exercise and update notes

--- triangle/triangle.zig
+++ triangle/triangle.zig
@@ -1,25 +1,24 @@
+pub const TriangleError = error{Invalid};
 pub const Triangle = struct {
     // This struct, as well as its fields and methods, needs to be implemented.
+    a: f64,
+    b: f64,
+    c: f64,
 
     pub fn init(a: f64, b: f64, c: f64) TriangleError!Triangle {
-        _ = a;
-        _ = b;
-        _ = c;
-        @compileError("please implement the init method");
+        if (a <= 0 or b <= 0 or c <= 0 or !(a + b >= c and b + c >= a and c + a >= b)) return TriangleError.Invalid;
+        return .{ .a = a, .b = b, .c = c };
     }
 
     pub fn isEquilateral(self: Triangle) bool {
-        _ = self;
-        @compileError("please implement the isEquilateral method");
+        return (self.a == self.b and self.b == self.c);
     }
 
     pub fn isIsosceles(self: Triangle) bool {
-        _ = self;
-        @compileError("please implement the isIsosceles method");
+        return (self.a == self.b or self.b == self.c or self.a == self.c);
     }
 
     pub fn isScalene(self: Triangle) bool {
-        _ = self;
-        @compileError("please implement the isScalene method");
+        return !(self.isEquilateral() or self.isIsosceles());
     }
 };
