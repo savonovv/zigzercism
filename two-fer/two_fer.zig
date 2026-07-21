48e057afb7664647827c69662fbcbe82796b1efc
Vladislav Savonov <vladislav.s@rakia.ai>
2026-07-21 14:57:23 +0400

    Complete two-fer exercise

---
+++ two-fer/two_fer.zig
@@ -0,0 +1,9 @@
+const std = @import("std");
+pub fn twoFer(buffer: []u8, name: ?[]const u8) ![]u8 {
+    return std.fmt.bufPrint(
+        buffer,
+        "One for {s}, one for me.",
+        .{name orelse "you"},
+    );
+}
+// One for you, one for me.
