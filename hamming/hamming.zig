1aaf7af1fa402431c1359173390553e40a12d1b1
Vladislav Savonov <vladislav.s@rakia.ai>
2026-07-22 15:27:26 +0400

    Complete grains and Hamming exercises

---
+++ hamming/hamming.zig
@@ -0,0 +1,13 @@
+pub const DnaError = error{ EmptyDnaStrands, UnequalDnaStrands };
+
+pub fn compute(first: []const u8, second: []const u8) DnaError!usize {
+    if (first.len == 0 or second.len == 0) return DnaError.EmptyDnaStrands;
+    if (first.len != second.len) return DnaError.UnequalDnaStrands;
+
+    var hamming_count: usize = 0;
+    for (first, second) |f, s| {
+        if (f != s) hamming_count += 1;
+    }
+
+    return hamming_count;
+}
