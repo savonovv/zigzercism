7bd6354e1e0a80da94678f0eede69967ad172645
Vladislav Savonov <vladislav.s@rakia.ai>
2026-08-17 08:11:49 +0400

    Simplify Matrix parsing

--- matrix/matrix.zig
+++ matrix/matrix.zig
@@ -1,29 +1,21 @@
 const std = @import("std");
-const mem = std.mem;
-/// Returns the selected row of the matrix.
-pub fn row(allocator: mem.Allocator, s: []const u8, index: i32) ![]i16 {
+pub fn row(allocator: std.mem.Allocator, s: []const u8, index: i32) ![]i16 {
     return parseMatrix(allocator, s, index, null);
 }
 
-/// Returns the selected column of the matrix.
-pub fn column(allocator: mem.Allocator, s: []const u8, index: i32) ![]i16 {
+pub fn column(allocator: std.mem.Allocator, s: []const u8, index: i32) ![]i16 {
     return parseMatrix(allocator, s, null, index);
 }
 
-pub fn parseMatrix(allocator: mem.Allocator, str: []const u8, row_n: ?i32, column_n: ?i32) ![]i16 {
+pub fn parseMatrix(allocator: std.mem.Allocator, str: []const u8, row_n: ?i32, column_n: ?i32) ![]i16 {
     var result: std.ArrayList(i16) = .empty;
     errdefer result.deinit(allocator);
     var rows = std.mem.splitScalar(u8, str, '\n');
-
     var i: i32 = 1;
-
     while (rows.next()) |row_e| : (i += 1) {
         var columns = std.mem.tokenizeScalar(u8, row_e, ' ');
         var n: i32 = 1;
-        while (columns.next()) |column_e| : (n += 1) {
-            const parsed_int = try std.fmt.parseInt(i16, column_e, 10);
-            if (row_n == i or column_n == n) try result.append(allocator, parsed_int);
-        }
+        while (columns.next()) |column_e| : (n += 1) if (row_n == i or column_n == n) try result.append(allocator, try std.fmt.parseInt(i16, column_e, 10));
     }
 
     return result.toOwnedSlice(allocator);
